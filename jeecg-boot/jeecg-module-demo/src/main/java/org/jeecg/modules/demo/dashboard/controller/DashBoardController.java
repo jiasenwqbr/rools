6cde810f721a6d077f6b9104066d67a93120ec6c
jiasenwqbr <jasonjiasen@gmail.com>
2024-07-24 18:07:48 +0800

    修改页面细节

---
+++ jeecg-boot/jeecg-module-demo/src/main/java/org/jeecg/modules/demo/dashboard/controller/DashBoardController.java
@@ -0,0 +1,7 @@
+package org.jeecg.modules.demo.dashboard.controller;/** 
+* @Description: 
+* @author: 贾森
+* @date: 2024年07月24日 15:57
+*/
+public class DashBoardController {
+}
---
+++ jeecg-boot/jeecg-module-demo/src/main/java/org/jeecg/modules/demo/dashboard/vo/CardData.java
@@ -0,0 +1,7 @@
+package org.jeecg.modules.demo.dashboard.vo;/** 
+* @Description: 
+* @author: 贾森
+* @date: 2024年07月24日 16:01
+*/
+public class CardData {
+}
